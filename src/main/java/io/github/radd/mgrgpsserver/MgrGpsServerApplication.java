fee1cb5f070923c291753b528f029a35a13fd4ed
Piotr Radziszewski <radnet.makis@o2.pl>
2019-02-22 18:27:49 +0100

    create project

---
+++ src/main/java/io/github/radd/mgrgpsserver/MgrGpsServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.radd.mgrgpsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgrGpsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgrGpsServerApplication.class, args);
+    }
+
+}
